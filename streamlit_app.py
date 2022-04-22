0a3d5995e4618ca6abbb6647111aa7f74ab972e9
testdevelopgabglez <104049174+testdevelopGabGlez@users.noreply.github.com>
2022-04-21 21:49:47 -0500

    Update streamlit_app.py
    Adding menu and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.write('Omega 3 and Blueberry')
+streamlit.write('Spinach')
+streamlit.write('Eggs')
